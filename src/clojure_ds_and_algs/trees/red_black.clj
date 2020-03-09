22548d78ace940a946a028c68b5356e41a260f4d
James <jamesgood626@gmail.com>
2020-03-09 13:33:24 -0400

    fixed the recursive update when adding nodes for red-black tree. Balancing is next.

--- src/clojure_ds_and_algs/trees/red_black.clj
+++ src/clojure_ds_and_algs/trees/red_black.clj
@@ -1,52 +1,71 @@
 (ns clojure-ds-and-algs.trees.red-black)
 
+
 (defn tree-node [val color]
   {:value val
    :color color
    :left-child nil
    :right-child nil})
 
-(defn add-tree-node [key val tree]
-  (let [node (key tree)
-        node-value (:value node)
-        color (if (= key :root) :black :red)
-        add-left (fn [node] (assoc node :left-child (add-tree-node :left-child val node)))
-        add-right (fn [node] (assoc node :right-child (add-tree-node :right-child val node)))]
-    (if (= node nil)
-      (assoc tree key (tree-node val color))
-      ((cond
-         (> node-value val) (assoc tree key (add-left node))
-         (< node-value val) (assoc tree key (add-right node))
-         :else (assoc tree key (tree-node val :red)))))))
 
-(defn update-nested-node [node key val]
+(defn add-tree-node [node key val]
+  "
+   After implementing add-node-to-tree and then re-reading the old implementation
+   of add-tree-node... realized that add-tree-node had superfluous logic.
+
+  add-node-to-tree does the work of finding a nil :left-child or :right-child
+  in which the new node value should be inserted.
+  "
+  (let [color (if (= key :root) :black :red)]
+    (assoc node key (tree-node val color))))
+
+
+(defn add-node-to-tree [node key val]
+  "
+  node can be passed in as any of the following:
+  1. The root of the tree, from which the root node will be accessed:
+     {:root {:value 100 :color :black :left-child nil :right-child nil}
+     in which case the key should be :root in order to access the map which
+     contains the node's :value.
+
+  2. A parent node, from which a child node will be accessed:
+     {:value 100 :color :black
+      :left-child {:value 80 :color :red :left-child nil :right-child nil}
+      :right-child nil
+     }
+     in which case the key should be either :left-child or :right-child
+     in order to access the map which contains the node's :value.
+
+  Those two cases comprise the set of possible (valid) inputs to the function.
+
+  val is the value which will be added to the tree as a new node.
+
+
+  The structure of accomodating nested updates with assoc:
+  (assoc tree :root
+         (assoc (:root tree) :right-child
+                (assoc (:right-child (:root tree)) :right-child 'WOOT')))
+
+  {:root
+   {:value 100, :color :black
+    :left-child nil
+    :right-child {:value 120, :color :red
+                  :left-child nil, :right-child 'WOOT'}}}
+  "
   (let [currently-visited-node (key node)
         node-val (:value currently-visited-node)
-        add-nested (fn [node direction-key] (assoc node key (add-tree-node direction-key val (key node))))
-        traverse (fn [node direction-key] (assoc node key (update-nested-node node direction-key val)))]
-    (cond
+        add-nested (fn [node direction-key] (assoc node key (add-tree-node currently-visited-node direction-key val)))
+        traverse (fn [node direction-key] (assoc node key (add-node-to-tree currently-visited-node direction-key val)))]
+    (if (and (= key :root) (= currently-visited-node nil))
+      (add-tree-node currently-visited-node :root val)
+      (cond
       ; Conditions where we update the nil node with our new node
-      (and (> node-val val) (= (:left-child node) nil)) (add-nested node :left-child)
-      (and (< node-val val) (= (:right-child node) nil)) (add-nested node :right-child)
+        (and (> node-val val) (= (:left-child currently-visited-node) nil)) (add-nested node :left-child)
+        (and (< node-val val) (= (:right-child currently-visited-node) nil)) (add-nested node :right-child)
       ; Conditions where we continue traversing
-      (> node-val val) (traverse node :left-child)
-      (< node-val val) (traverse node :right-child))))
+        (> node-val val) (traverse node :left-child)
+        (< node-val val) (traverse node :right-child)))))
 
-; (if (= (key node) nil)
-;   (assoc node key (tree-node val :red))
-;   (cond
-;     (and (> (:value node) val) (= (:left-child node) nil)) (assoc (:left-child node) :left-child (update-nested-node :left-child val node))
-;     (and (< (:value node) val) (= (:right-child node) nil)) (assoc (:right-child node) :right-child (update-nested-node :right-child val node))
-
-;     (> (:value node) val) (assoc (:left-child node) key (update-nested-node :left-child val node))
-;     (< (:value node) val) (assoc (:right-child node) key (update-nested-node :right-child val node))
-;     :else (assoc node key (tree-node val :red))))
-
-; (defn test-case [x]
-;   (cond
-;     (< x 10) (assoc {:root nil} :root "Is less than 10")
-;     (and (> x 10) (< x 20)) (assoc {:root nil} :root "Is greater than 10 and less than 20")
-;     (>= x 20) (assoc {:root nil} :root "Is greater than or equal to 20")))
 
 ; (defn create-tree [[x & xs] tree]
 ;   (if (= xs 0)
--- test/clojure_ds_and_algs/trees/red_black_test.clj
+++ test/clojure_ds_and_algs/trees/red_black_test.clj
@@ -8,23 +8,61 @@
 
 (deftest insert-root-if-tree-empty
   (testing "add-tree-node inserts a root node if tree passed in is empty"
-    (is (= (rb/add-tree-node :root 100 {:root nil}) {:root {:value 100 :color :black :left-child nil :right-child nil}}))))
+    (let [tree {:root nil}
+          key :root
+          val 100]
+      (is (= (rb/add-tree-node tree key val) {:root {:value 100 :color :black :left-child nil :right-child nil}})))))
 
-; (deftest insert-root-if-tree-node-present
-;   (testing "add-tree-node inserts a node if root is already present"
-;     (let [tree (rb/add-tree-node :root 100 {:root nil})]
-;       (is (= (rb/add-tree-node :root 200 tree) {:root {:value 200 :color :black}})))))
+(deftest add-node-to-tree-nested
+  (testing "add-node-to-tree adds a node to a tree w/ nested nodes, returning only the subtree which the newly
+            added node was inserted into"
+    (let [right-node {:value 110 :color :red :left-child nil :right-child nil}
+          left-node {:value 90 :color :red :left-child nil :right-child nil}
+          tree {:root {:value 100 :color :black :left-child left-node :right-child right-node}}
+          new-val 120
+          new-node {:value 120 :color :red :left-child nil :right-child nil}
+          nested-node-to-update (:right-child (:root tree))
+          expected-tree {:value 110, :color :red, :left-child nil, :right-child new-node}]
+      (is (= (rb/add-tree-node nested-node-to-update :right-child new-val) expected-tree)))))
 
-(deftest nested-update-right
-  (testing "update-nested-node adds a node with value greater than the root's value as the root's right-child"
+(deftest add-node-to-tree-root
+  (testing "add-node-to-tree adds a node to an empty tree as the root node"
+    (let [tree {:root nil}
+          new-val 120
+          new-node {:value 120 :color :black :left-child nil :right-child nil}
+          expected-tree {:root new-node}]
+      (is (= (rb/add-node-to-tree tree :root 120) expected-tree)))))
+
+(deftest add-node-to-tree-right
+  (testing "add-node-to-tree adds a node with value greater than the root's value as the root's right-child"
     (let [tree {:root {:value 100 :color :black :left-child nil :right-child nil}}
           new-node {:value 120 :color :red :left-child nil :right-child nil}
           expected-tree {:root {:value 100 :color :black :left-child nil :right-child new-node}}]
-      (is (= (rb/update-nested-node tree :root 120) expected-tree)))))
+      (is (= (rb/add-node-to-tree tree :root 120) expected-tree)))))
 
-(deftest nested-update-left
-  (testing "update-nested-node adds a node with value less than the root's value as the root's left-child"
+(deftest add-node-to-tree-left
+  (testing "add-node-to-tree adds a node with value less than the root's value as the root's left-child"
     (let [tree {:root {:value 100 :color :black :left-child nil :right-child nil}}
           new-node {:value 80 :color :red :left-child nil :right-child nil}
           expected-tree {:root {:value 100 :color :black :left-child new-node :right-child nil}}]
-      (is (= (rb/update-nested-node tree :root 80) expected-tree)))))
+      (is (= (rb/add-node-to-tree tree :root 80) expected-tree)))))
+
+(deftest add-node-to-tree-subsequent-entries
+  (testing "add-node-to-tree adds multiple nodes to the tree"
+    (let [tree {:root {:value 100 :color :black :left-child nil :right-child nil}}
+          first-new-node {:value 80 :color :red :left-child nil :right-child nil}
+          second-new-node {:value 70 :color :red :left-child nil :right-child nil}
+          expected-tree {:root {:value 100 :color :black :left-child first-new-node :right-child nil}}
+          tree-with-one-node (rb/add-node-to-tree tree :root 80)
+          tree-with-two-nodes (rb/add-node-to-tree tree-with-one-node :root 70)]
+      (is (= (rb/add-node-to-tree tree-with-two-nodes :root 60) {:root {:value 100, :color :black, :left-child {:value 80, :color :red, :left-child {:value 70, :color :red, :left-child {:value 60, :color :red, :left-child nil, :right-child nil}, :right-child nil}, :right-child nil}, :right-child nil}})))))
+
+(deftest maintain-red-black-invariants
+  (testing "starting to implement red-black tree invariant adherance"
+    (let [nodes-to-add [10 20 30 40 50 60 70 80 90 100]
+          work-fn (fn [acc val]
+                    (println "acc is")
+                    (println acc)
+                    (rb/add-node-to-tree acc :root val))
+          tree (reduce work-fn {:root nil} nodes-to-add)]
+      (is (= tree "WHAAT")))))
